47bc9f0aea8261465d52f3183d9b9bc2843d05d7
Dj4t9n <pandeyjatin47@gmail.com>
2021-06-05 23:50:52 +0530

    add screen and frame rate

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #define PI M_PI
 #define SCR_WIDTH 80
 #define SCR_HEIGHT 22
-#define DELAY 3e3
+#define DELAY 3e4
 
 const char* luminance = ".,-~:;=!*#$@";
 
@@ -27,16 +27,16 @@ void move_curs_to_home(void){
 
 int main() {
     float A = 0, B = 0;
+    long long int frame;
     float i, j;
     float z_buff[SCR_HEIGHT*SCR_WIDTH];
     char output[SCR_HEIGHT*SCR_WIDTH];
     clr_scr();
     while(true) {
-        memset(output, ' ', SCR_HEIGHT*SCR_WIDTH);
+        memset(output, '.', SCR_HEIGHT*SCR_WIDTH);
         memset(z_buff, '\0', SCR_HEIGHT*SCR_WIDTH*4);
         for(j=0; j < 2*PI; j += 0.07) {
             for(i=0; i < 2*PI; i += 0.02) {
-                printf("Computing");
                 float sini = sin(i);
                 float cosj = cos(j);
                 float sinA = sin(A);
@@ -61,11 +61,12 @@ int main() {
 
         move_curs_to_home();
         for(int k = 0; k < SCR_WIDTH*SCR_HEIGHT; k++) {
-            usleep(DELAY);
             putchar(k % SCR_WIDTH ? output[k] : '\n');
             A += 4e-5;
             B += 2e-5;
         }
+        printf("%lld", ++frame);
+        usleep(DELAY);
     }
     return 0;
 }
